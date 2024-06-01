31cfcc3fc78c558433808b60b9f055240e7f1555
njenga-david <123449936+njenga-david@users.noreply.github.com>
2024-06-01 20:42:16 +0300

    javascript commits

---
+++ func.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn')
+
+const sayHi = function(){
+    console.log('hello world')
+}
+
+btn.addEventListener('click', function(){
+    console.log('hello world')
+})
---
+++ ref.js
@@ -0,0 +1,7 @@
+let names =[
+    'john', 'kamau', 'winnie', 'janet', 
+]
+names.unshift(3,'Anne')
+// console.log(names.pop())
+console.log(names)
+
